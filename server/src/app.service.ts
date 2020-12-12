27984c44f789e54f3a4fd779acf89e49fecd3189
Marek Kustosz <markust71@hotmail.com>
2020-12-12 13:38:19 +0100

    query pbsim

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -8,8 +8,8 @@ export class AppService {
         const config = {
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
-            server: `${process.env.DB_HOST}`,
-            database: 'ERPXL_PBSiM',
+            server: process.env.DB_HOST,
+            database: process.env.DB_DATABASE,
             pool: {
                 max: 10,
                 min: 0,
@@ -19,15 +19,29 @@ export class AppService {
                 enableArithAbort: true,
             },
         };
+        console.log('config:', config);
         const sqlQuery = async () => {
             try {
                 console.log('Connecting...');
                 await sql.connect(config);
                 try {
                     console.log('Querying...');
-                    const result = await sql.query(
-                        `select * from CDN.KntKarty WHERE Knt_Nazwa1 LIKE '%MITAX%'`,
-                    );
+                    const query = `SELECT TraNag.TrN_TrNRok, TraNag.TrN_TrNMiesiac, TraNag.TrN_TrNTyp, TraNag.TrN_TrNSeria, 
+                                        TraNag.TrN_TrNNumer, TraNag.TrN_DokumentObcy, KntKarty.Knt_Akronim, KntKarty.Knt_Nazwa1, 
+                                        KntKarty.Knt_Miasto, KntKarty.Knt_Nip, TraNag.TrN_NettoP, TraNag.TrN_VatP, 
+                                        TraNag.TrN_GIDTyp, TraNag.TrN_GIDNumer, 
+                                        KntKarty.Knt_Akronim+KntKarty.Knt_Nazwa1+KntKarty.Knt_Miasto+KntKarty.Knt_Ulica+KntKarty.Knt_Nip AS 'klucz', 
+                                        CDN.TSToDate(TraNag.TrN_Data2, 0) AS 'DataW', CDN.TSToDate(TraNag.TrN_Data3, 0) AS 'DataZ', 
+                                        CDN.NumerDokumentu(TraNag.TrN_GIDTyp,TraNag.TrN_SpiTyp,TraNag.TrN_TrnTyp,TraNag.TrN_TrnNumer,TraNag.TrN_TrnRok,TraNag.TrN_TrnSeria,TraNag.TrN_TrnMiesiac) AS 'ident'
+                                   FROM CDN.KntKarty KntKarty, CDN.TraNag TraNag
+                                   WHERE TraNag.TrN_KntNumer = KntKarty.Knt_GIDNumer 
+                                        AND ((TraNag.TrN_Dziennik Like '%') 
+                                            AND (TraNag.TrN_DokumentObcy Like '%'+''+'%') 
+                                            AND (KntKarty.Knt_Akronim+KntKarty.Knt_Nazwa1+KntKarty.Knt_Miasto+KntKarty.Knt_Ulica+KntKarty.Knt_Nip Like '%'+''+'%') 
+                                            AND (TraNag.TrN_TrNSeria+'/'+CDN.NumerDokumentu(TraNag.TrN_GIDTyp,TraNag.TrN_SpiTyp,TraNag.TrN_TrnTyp,TraNag.TrN_TrnNumer,TraNag.TrN_TrnRok,TraNag.TrN_TrnSeria,TraNag.TrN_TrnMiesiac) Like '%'+''+'%') 
+                                            AND (TraNag.TrN_TrNRok>=2020))
+                                   ORDER BY TraNag.TrN_TrNRok, TraNag.TrN_TrNMiesiac, TraNag.TrN_TrNTyp, TraNag.TrN_TrNSeria, TraNag.TrN_TrNNumer`;
+                    const result = await sql.query(query);
                     console.log('result:', result);
                 } catch {
                     (error) =>
